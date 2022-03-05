bc624966e675e447a2e9da69d373cf3302ebe552
Smile <bsaimukeshreddy820@gmail.com>
2022-03-05 19:26:26 +0530

    Dont know why validation is failing

--- src/com/saimukesh/datastructers/ds/arrays/algoexpert/targetSubsequnece.java
+++ src/com/saimukesh/datastructers/ds/arrays/algoexpert/targetSubsequnece.java
@@ -2,32 +2,31 @@ package com.saimukesh.datastructers.ds.arrays.algoexpert;
 import java.util.*;
 public class targetSubsequnece {
 
-
-        public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-            // Write your code here.
-            int z=0;
-            for(int i =0; i<array.size()-1;i++){
-               int a= array.get(i) ;
-                System.out.println(a);
-
-                for(int j=0;j<array.size()-1;j++){
-                  int  b= array.get(i);
-                  System.out.println(b);
-                    if(a==b){
-                        z++;
-
-                    }
-
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+        int z=0;
+        for(int i =0; i<array.size()-1;i++){
+            for(int j=0;j<array.size()-1;j++){
+                if(array.get(i)==array.get(i)){
+                    z++;
                 }
+
             }
-            if(z== sequence.size()){return true;}
-            return false;
+
         }
+        if(z == sequence.size()){
+            return true;
+        }
+        return false;
+    }
+
+
+
 
 
     public static void main(String[] args) {
         List<Integer> array = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
-        List<Integer> sequence =Arrays.asList(1, 6, -1, 10);
+        List<Integer> sequence =Arrays.asList(5, 26, 22, 8);
        isValidSubsequence(array,sequence);
 
     }
