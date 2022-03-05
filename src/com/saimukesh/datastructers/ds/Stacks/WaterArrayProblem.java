688bdf0add60395933e8f9d7bfdbe8c8cbe82d5f
Smile <bsaimukeshreddy820@gmail.com>
2022-03-05 23:04:59 +0530

    trappingRainWaterProblem

---
+++ src/com/saimukesh/datastructers/ds/Stacks/WaterArrayProblem.java
@@ -0,0 +1,16 @@
+package com.saimukesh.datastructers.ds.Stacks;
+
+public class WaterArrayProblem {
+    public static void main(String[] args) {
+
+    }
+        // arr: input array
+        // n: size of array
+        // Function to find the trapped water between the blocks.
+        static long trappingWater(int arr[], int n) {
+
+            return 0;
+        }
+
+
+    }
