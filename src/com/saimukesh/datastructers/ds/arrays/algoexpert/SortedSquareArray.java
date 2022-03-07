9e383c05c837995c8a5ce331e3a7e32d0c5682e7
Smile <bsaimukeshreddy820@gmail.com>
2022-03-07 21:08:29 +0530

    UnnnecessaryCommit

---
+++ src/com/saimukesh/datastructers/ds/arrays/algoexpert/SortedSquareArray.java
@@ -0,0 +1,12 @@
+package com.saimukesh.datastructers.ds.arrays.algoexpert;
+
+public class SortedSquareArray {
+
+    public int[] sortedSquaredArray(int[] array) {
+        // Write your code here.
+        int positivePointer =0;
+        int negativePointer =0; // to find the
+//kjsyialkmaka
+        return new int[] {};
+    }
+}
