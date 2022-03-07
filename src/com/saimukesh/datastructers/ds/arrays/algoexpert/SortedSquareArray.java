829bc3a73c4c822c3942e5088e57f82c1aa528c7
Smile <bsaimukeshreddy820@gmail.com>
2022-03-07 22:46:52 +0530

    SortedSquareArrayUsinginbuiltArrayMethods

--- src/com/saimukesh/datastructers/ds/arrays/algoexpert/SortedSquareArray.java
+++ src/com/saimukesh/datastructers/ds/arrays/algoexpert/SortedSquareArray.java
@@ -1,12 +1,26 @@
 package com.saimukesh.datastructers.ds.arrays.algoexpert;
 
+import java.util.Arrays;
+
 public class SortedSquareArray {
 
-    public int[] sortedSquaredArray(int[] array) {
+    public int[] sortedSquaresUsingInbuiltMethods(int[] nums) {
+
+        int arr[]=new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            arr[i]=(int)Math.pow(Math.abs(nums[i]),2);
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    public int[] sortedSquaredArrayUsingTwoPointerMethod(int[] array) {
         // Write your code here.
         int positivePointer =0;
-        int negativePointer =0; // to find the
-//kjsyialkmaka
-        return new int[] {};
+        int negativePointer =0;
+        while(negativePointer<array.length && array[negativePointer]<0){negativePointer++;}
+
+                return new int[] {};
     }
 }
